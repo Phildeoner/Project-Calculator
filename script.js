74e13b61052676f93eb4112dd31fed2f077a51c3
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2023-01-20 11:29:33 +0100

    Create performOperation function to perform selected operations on input

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function performEvent(buttons) {
           }, 1000);
           return;
         }
-        calculate();
+        performOperation();
         previousOperator = "";
         previousResult = 0;
       } else {
@@ -45,3 +45,36 @@ function performEvent(buttons) {
     }
 }
 buttons.forEach((button) => {button.addEventListener("click", performEvent)});
+
+function performOperation() {
+    let result = 0;
+    let currentNumber = parseFloat(firstNumber);
+    if (previousOperator !== "") {
+        currentNumber = previousResult;
+    }
+    if (operator === "+") {
+        result = currentNumber + parseFloat(secondNumber);
+    } else if (operator === "-") {
+        result = currentNumber - parseFloat(secondNumber);
+    } else if (operator === "*") {
+        result = currentNumber * parseFloat(secondNumber);
+    } else if (operator === "/") {
+        if (secondNumber == 0) {
+            // add error class to display element
+            display.classList.add("error");
+            setTimeout(() => {
+            // remove error class after 1 sec
+            display.classList.remove("error");
+            }, 1000);
+            return;
+        }
+        result = currentNumber / parseFloat(secondNumber);
+    }
+    previousOperator = operator;
+    previousResult = result;
+    firstNumber = "";
+    secondNumber = "";
+    operatorSet = false;
+    operator = "";
+    display.value = result;
+}
